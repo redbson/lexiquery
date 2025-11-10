1073d96f5a16aee14110d77d9c4d9b3422ac4262
Reg <redbson@gmail.com>
2025-11-10 21:33:23 +0800

    Init.

---
+++ src/lexiquery/__init__.py
@@ -0,0 +1,5 @@
+from .core import LexiQuery, LexiQueryExpression, test_lexi_grammar
+from .exceptions import GrammarError
+
+__all__ = ["LexiQuery", "LexiQueryExpression", "GrammarError", "test_lexi_grammar"]
+__version__ = "0.2.0"
